00374fb273a46d548575990ac785aa9198c3f132
Taylor Davis <td916215@ohio.edu>
2018-10-05 08:34:46 -0400

    first commit, modified from HW3_1

--- main.c
+++ main.c
@@ -1,61 +1,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void gradeCheck(float);
+
 int main()
 {
     float yourGrade;
 
+    // prompt user for grade number
     printf("Enter your score: ");
     scanf("%f", &yourGrade);
 
-    if (yourGrade >= 0 && yourGrade <= 59.9)
+    gradeCheck(yourGrade);
+
+    return 0;
+}
+
+void gradeCheck(float enteredValue) // give letter grade based on number entered
+{
+    if (enteredValue >= 0 && enteredValue <= 59.9)
     {
         printf("You got an F");
     }
-    else if (yourGrade >= 60 && yourGrade <= 62.9)
+    else if (enteredValue >= 60 && enteredValue <= 62.9)
     {
         printf("You got a D-");
     }
-    else if (yourGrade >= 63 && yourGrade <= 66.9)
+    else if (enteredValue >= 63 && enteredValue <= 66.9)
     {
         printf("You got a D");
     }
-    else if (yourGrade >= 67 && yourGrade <= 69.9)
+    else if (enteredValue >= 67 && enteredValue <= 69.9)
     {
         printf("You got a D+");
     }
-    else if (yourGrade >= 70 && yourGrade <= 72.9)
+    else if (enteredValue >= 70 && enteredValue <= 72.9)
     {
         printf("You got a C-");
     }
-    else if (yourGrade >= 73 && yourGrade <= 76.9)
+    else if (enteredValue >= 73 && enteredValue <= 76.9)
     {
         printf("You got a C");
     }
-    else if (yourGrade >= 77 && yourGrade <= 79.9)
+    else if (enteredValue >= 77 && enteredValue <= 79.9)
     {
         printf("You got a C+");
     }
-    else if (yourGrade >= 80 && yourGrade <= 82.9)
+    else if (enteredValue >= 80 && enteredValue <= 82.9)
     {
         printf("You got a B-");
     }
-    else if (yourGrade >= 83 && yourGrade <= 86.9)
+    else if (enteredValue >= 83 && enteredValue <= 86.9)
     {
         printf("You got a B");
     }
-    else if (yourGrade >= 87 && yourGrade <= 89.9)
+    else if (enteredValue >= 87 && enteredValue <= 89.9)
     {
         printf("You got a B+");
     }
-    else if (yourGrade >= 90 && yourGrade <= 93.9)
+    else if (enteredValue >= 90 && enteredValue <= 93.9)
     {
         printf("You got an A-");
     }
-    else if (yourGrade >= 94 && yourGrade <= 100)
+    else if (enteredValue >= 94 && enteredValue <= 100)
     {
         printf("You got an A");
     }
-
-    return 0;
 }
